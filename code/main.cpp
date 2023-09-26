1ef0708a1e41ff823ab64ad2ca2e354df80ef708
tomascabrerizo <tomascabrerizo97@gmail.com>
2023-09-26 17:30:16 -0300

    write animations frames to file

--- code/main.cpp
+++ code/main.cpp
@@ -1,4 +1,5 @@
 #include <assimp/anim.h>
+#include <assimp/matrix4x4.h>
 #include <assimp/types.h>
 #include <stdio.h>
 #include <assert.h>
@@ -17,10 +18,91 @@
 #define TWEEN_SKELETON   (1 << 1)
 #define TWEEN_ANIMATIONS (1 << 2)
 
+
+static void write_key_frame(unsigned int id, aiVectorKey position_key, aiQuatKey rotation_key, aiVectorKey scaling_key, FILE* file) {
+    assert(position_key.mTime == rotation_key.mTime && position_key.mTime == rotation_key.mTime);
+    
+    float time = position_key.mTime / 1000.0f;
+
+    fwrite(&id, sizeof(unsigned int), 1, file);
+    fwrite(&time, sizeof(float), 1, file);
+    
+    fwrite(&position_key.mValue.x, sizeof(float), 1, file);
+    fwrite(&position_key.mValue.y, sizeof(float), 1, file);
+    fwrite(&position_key.mValue.z, sizeof(float), 1, file);
+
+    fwrite(&rotation_key.mValue.w, sizeof(float), 1, file);
+    fwrite(&rotation_key.mValue.x, sizeof(float), 1, file);
+    fwrite(&rotation_key.mValue.y, sizeof(float), 1, file);
+    fwrite(&rotation_key.mValue.z, sizeof(float), 1, file);
+
+    fwrite(&scaling_key.mValue.x, sizeof(float), 1, file);
+    fwrite(&scaling_key.mValue.y, sizeof(float), 1, file);
+    fwrite(&scaling_key.mValue.z, sizeof(float), 1, file);
+
+}
+
+static void write_string(aiString string, FILE *file) {
+    fwrite(&string.length, sizeof(unsigned int), 1, file);
+    fwrite(string.data, sizeof(char), string.length, file);
+}
+
+static void write_matrix(aiMatrix4x4 matrix, FILE *file) {
+
+    fwrite(&matrix.a1, sizeof(float), 1, file);
+    fwrite(&matrix.a2, sizeof(float), 1, file);
+    fwrite(&matrix.a3, sizeof(float), 1, file);
+    fwrite(&matrix.a4, sizeof(float), 1, file);
+
+    fwrite(&matrix.b1, sizeof(float), 1, file);
+    fwrite(&matrix.b2, sizeof(float), 1, file);
+    fwrite(&matrix.b3, sizeof(float), 1, file);
+    fwrite(&matrix.b4, sizeof(float), 1, file);
+
+    fwrite(&matrix.c1, sizeof(float), 1, file);
+    fwrite(&matrix.c2, sizeof(float), 1, file);
+    fwrite(&matrix.c3, sizeof(float), 1, file);
+    fwrite(&matrix.c4, sizeof(float), 1, file);
+
+    fwrite(&matrix.d1, sizeof(float), 1, file);
+    fwrite(&matrix.d2, sizeof(float), 1, file);
+    fwrite(&matrix.d3, sizeof(float), 1, file);
+    fwrite(&matrix.d4, sizeof(float), 1, file);
+}
+
+static void count_child_nodes(aiNode *node, unsigned int *num_bones) {
+    ++(*num_bones);
+    for(unsigned int i = 0; i < node->mNumChildren; ++i) {
+        aiNode *child = node->mChildren[i];
+        count_child_nodes(child, num_bones);
+    }
+}
+
+static void write_skeleton_node(aiNode *node, FILE *file, unsigned int parent_offset, unsigned int *current_offset) {
+    
+    unsigned int offset = *current_offset;
+    ++(*current_offset);
+     
+    fwrite(&parent_offset, sizeof(unsigned int), 1, file);
+    write_matrix(node->mTransformation, file);
+
+    printf("%d) node: %s, parent offset: %d\n", offset, node->mName.C_Str(), parent_offset);
+
+    for(unsigned int i = 0; i < node->mNumChildren; ++i) {
+        aiNode *child = node->mChildren[i];
+        write_skeleton_node(child, file, offset, current_offset);
+    }
+}
+
 static void write_skeleton_hierarchy(aiNode *node, FILE *file) {
-    (void)node; (void)file;
-    // TODO: Write skeleton hierarchy to the file
-    assert(!"unimplemented codepath");
+
+    unsigned int num_bones = 0;
+    count_child_nodes(node, &num_bones);
+    printf("Number of bones: %d\n", num_bones);
+    fwrite(&num_bones, sizeof(unsigned int), 1, file);
+    
+    unsigned int start_offset = 0;
+    write_skeleton_node(node, file, (unsigned int)-1, &start_offset);
 }
 
 static void print_bones(aiNode *node, unsigned int *index) {
@@ -143,20 +225,43 @@ int main(void) {
     assert(root_node);
     
     printf("Skeleton name: %s\n", root_node->mName.C_Str());
-    fwrite(&root_node->mName.length, sizeof(unsigned int), 1, file);
-    fwrite(&root_node->mName, sizeof(char), root_node->mName.length, file);
-
+    write_string(root_node->mName, file);
     write_skeleton_hierarchy(root_node, file);
 
     printf("Number of animations: %d\n", scene->mNumAnimations);
     fwrite(&scene->mNumAnimations, sizeof(unsigned int), 1, file);
     
-    for(unsigned int i = 0; i < scene->mNumAnimations; ++i) {
-        aiAnimation *animation = scene->mAnimations[i];
-        (void)animation;
-        // TODO: Write animations keyframes for each bone
-        assert(!"unimplemented codepath");
+
+    for(unsigned int animation_index = 0; animation_index < scene->mNumAnimations; ++animation_index) {
+        aiAnimation *animation = scene->mAnimations[animation_index];
+
+        /* NOTE: The animations are spected to have to same number of keyframes for each bone */
+        assert(animation->mNumChannels > 0);
+        unsigned int num_keyframes = animation->mChannels[0]->mNumPositionKeys; 
+        assert(num_keyframes == animation->mChannels[0]->mNumRotationKeys);
+
+        float duration = animation->mDuration/1000.0f;
+        printf("Animation name: %s, duration: %f\n", animation->mName.C_Str(), duration);
+        write_string(animation->mName, file);
+        fwrite(&duration, sizeof(float), 1, file);
+        fwrite(&num_keyframes, sizeof(unsigned int), 1, file);
+
+        for(unsigned int keyframe_index = 0; keyframe_index < num_keyframes; ++keyframe_index) {
+            
+            fwrite(&animation->mNumChannels, sizeof(unsigned int), 1, file);
+
+            for(unsigned int bone_index = 0; bone_index < animation->mNumChannels; ++bone_index) {
+
+                aiNodeAnim *node = animation->mChannels[bone_index];
+                unsigned int id = find_bone_id(root_node, node->mNodeName);
+                write_key_frame(id, node->mPositionKeys[keyframe_index], node->mRotationKeys[keyframe_index], node->mScalingKeys[keyframe_index], file);
+            }
+        }
     }
+
+    fclose(file);
+
+    printf("Animation file write perfectly\n");
 
     return 0;
 }
@@ -165,7 +270,7 @@ int main2(void) {
 
     Assimp::Importer importer;
 
-    const aiScene *scene = importer.ReadFile("./data/model.dae", 
+    const aiScene *scene = importer.ReadFile("./data/Angry.dae", 
             aiProcess_Triangulate           |
             aiProcess_JoinIdenticalVertices |
             aiProcess_SortByPType); 
@@ -251,8 +356,7 @@ int main2(void) {
         assert(root_node);
         
         printf("Skeleton name: %s\n", root_node->mName.C_Str());
-        fwrite(&root_node->mName.length, sizeof(unsigned int), 1, file);
-        fwrite(&root_node->mName, sizeof(char), root_node->mName.length, file);
+        write_string(root_node->mName, file);
 
         unsigned int last_num_bones = 0;
         for (unsigned int i = 0; i < scene->mNumMeshes; ++i) {
@@ -265,26 +369,7 @@ int main2(void) {
                 fwrite(&id, sizeof(unsigned int), 1, file);
                 fwrite(&bone->mNumWeights, sizeof(unsigned int), 1, file);
                 fwrite(bone->mWeights, sizeof(float), sizeof(float)*bone->mNumWeights, file);
-                
-                fwrite(&bone->mOffsetMatrix.a1, sizeof(float), 1, file);
-                fwrite(&bone->mOffsetMatrix.a2, sizeof(float), 1, file);
-                fwrite(&bone->mOffsetMatrix.a3, sizeof(float), 1, file);
-                fwrite(&bone->mOffsetMatrix.a4, sizeof(float), 1, file);
-
-                fwrite(&bone->mOffsetMatrix.b1, sizeof(float), 1, file);
-                fwrite(&bone->mOffsetMatrix.b2, sizeof(float), 1, file);
-                fwrite(&bone->mOffsetMatrix.b3, sizeof(float), 1, file);
-                fwrite(&bone->mOffsetMatrix.b4, sizeof(float), 1, file);
-
-                fwrite(&bone->mOffsetMatrix.c1, sizeof(float), 1, file);
-                fwrite(&bone->mOffsetMatrix.c2, sizeof(float), 1, file);
-                fwrite(&bone->mOffsetMatrix.c3, sizeof(float), 1, file);
-                fwrite(&bone->mOffsetMatrix.c4, sizeof(float), 1, file);
-
-                fwrite(&bone->mOffsetMatrix.d1, sizeof(float), 1, file);
-                fwrite(&bone->mOffsetMatrix.d2, sizeof(float), 1, file);
-                fwrite(&bone->mOffsetMatrix.d3, sizeof(float), 1, file);
-                fwrite(&bone->mOffsetMatrix.d4, sizeof(float), 1, file);
+                write_matrix(bone->mOffsetMatrix, file);
 
                 printf("%d) bone: %s bone index: %d\n", last_num_bones+j, bone->mName.C_Str(), id);
             
@@ -293,5 +378,7 @@ int main2(void) {
         }
     }
     
+    fclose(file);
+
     return 0;
 }
